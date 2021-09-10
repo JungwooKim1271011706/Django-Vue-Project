45372e5d23dfefd9022ad60220cdb96df2022002
Jungwoo Kim <wjddn0316@DESKTOP-6D3BTM0.localdomain>
2021-09-11 00:59:09 +0900

    Django-Vue axios post apply

--- django/crinity_django/crinity/views.py
+++ django/crinity_django/crinity/views.py
@@ -1,25 +1,23 @@
+import json
+
 from django.http.response import JsonResponse
 from django.shortcuts import render
-import json
+from django.views.decorators.csrf import csrf_protect
+
 
 # .통합테스트를 할 때 엑셀파일을 자동으로 만들어주는 프로그램의 View
 def crinity(request):
     return render(request, 'crinity/crinity_testdata.html', {})
 
+@csrf_protect
 def getTestData(request):
     # 요청 객체에서 데이터 바인딩하기
     print("통신 성공")
     data = json.loads(request.body)
-    #row = request.POST["row"]
-    #column = request.POST['column']
-    #domain = request.POST['domain']
     row = data["row"]
     print("row >>> ", row)
     data = {"row" : row}
 
-    #print("column >>> ", column)
-    #print("domain >>> ", domain)
-    #return render(request, 'crinity/crinity_testdata.html', {"row" : row + "1"})
     return JsonResponse(data)
 
 # 지도 API를 가지고 와서 즐겨찾기처럼 기록할 수 있는 함수
